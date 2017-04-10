c95a612d99220ae6c65c7f750d6852329600b00d
Megaju <julien_malle@outlook.fr>
2017-04-10 16:43:41 +0200

    favicon

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ $total = 0;
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="icon" type="image/png" href="images/cart.png" />
   <title>ShopList</title>
   <!-- Font icons -->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
