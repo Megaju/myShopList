683aeecfc030b756183542d953fda3ea08316c00
Megaju <julien_malle@outlook.fr>
2017-04-10 19:16:31 +0200

    new db example

--- list.sql
+++ list.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Lun 10 Avril 2017 à 18:09
+-- Généré le :  Lun 10 Avril 2017 à 19:16
 -- Version du serveur :  10.0.29-MariaDB-0ubuntu0.16.04.1
 -- Version de PHP :  7.0.15-0ubuntu0.16.04.4
 
@@ -43,11 +43,12 @@ CREATE TABLE `list` (
 
 INSERT INTO `list` (`id`, `name`, `amount`, `kilos`, `gramme`, `euro`, `centim`) VALUES
 (19, 'Patates', 1, 1, 0, 3, 99),
-(21, 'Camember', 1, 0, 100, 2, 0),
-(22, 'Pains grillés blé complet Carrefour', 2, 0, 220, 2, 0),
+(21, 'Camember', 1, 0, 100, 2, 1),
+(22, 'Pains grillés blé complet Carrefour', 2, 0, 220, 2, 2),
 (23, 'Beurre', 1, 0, 250, 2, 0),
-(24, 'Pates', 4, 1, 0, 3, 0),
-(25, 'Tomates cerises', 2, 0, 50, 1, 15);
+(24, 'Pates', 4, 1, 0, 3, 97),
+(25, 'Tomates cerises', 2, 0, 50, 1, 15),
+(26, 'éponge', 4, 0, 0, 0, 10);
 
 --
 -- Index pour les tables exportées
@@ -67,7 +68,7 @@ ALTER TABLE `list`
 -- AUTO_INCREMENT pour la table `list`
 --
 ALTER TABLE `list`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
